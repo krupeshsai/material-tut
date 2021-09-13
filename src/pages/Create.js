ce56a05fcb8b5a3ab71e7cd97c3f1c303f54d2a4
krupeshsai <krupeshsai@gmail.com>
2021-09-13 14:37:00 +0530

    makeStyles

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,31 +2,45 @@ import React from 'react'
 import {Typography} from "@material-ui/core"
 import {Button} from '@material-ui/core';
 import { Container } from '@material-ui/core';
-import AdbOutlinedIcon from '@material-ui/icons/AdbOutlined';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  btn:{
+    fontSize: 10,
+    backgroundColor:"green",
+    "&:hover" :{
+      backgroundColor:"blue"
+    }
+  },
+  title :{
+    textDecoration: "underline",
+    marginBottom: 20
+  }
+})
+
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
     <Container>
       <Typography 
+      className={classes.title}
       variant ="h6"
        color = "textSecondary" 
        component = "h2"
       gutterBottom>Create a New Note</Typography>
 
       <Button 
+      className={classes.btn}
       onClick= {()=>console.log("You clicked me")}
       type ="submit" 
       color ="secondary"
-      variant="outlined"
+      variant="contained"
       disableElevation
-    
       endIcon={<KeyboardArrowRightIcon/>}>Submit</Button>
 
-      {/* icons */}
-      <br />
-      <AdbOutlinedIcon />
-      <AdbOutlinedIcon color = "secondary" fontSize="large"/>
 
     </Container>
   )
