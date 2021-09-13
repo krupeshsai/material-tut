0513ae85b53d71ec9fa316f898a19ba0f50f0870
krupeshsai <krupeshsai@gmail.com>
2021-09-13 13:50:44 +0530

    buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import {Typography} from "@material-ui/core"
+import {Button} from '@material-ui/core';
+import { ButtonGroup } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 
 export default function Create() {
   return (
-    <div>
-      <Typography variant ="h1" color = "primary" align = "center">Create a New Note</Typography>
-    </div>
+    <Container>
+      <Typography 
+      variant ="h6"
+       color = "textSecondary" 
+       component = "h2"
+      gutterBottom>Create a New Note</Typography>
+      <Button 
+      onClick= {()=>console.log("You clicked me")}
+      type ="submit" 
+      color ="secondary"
+      variant="outlined"
+      disableElevation>Submit</Button>
+
+      {/* <ButtonGroup color ="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
