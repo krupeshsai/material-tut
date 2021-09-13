349d243d2291cdd334339cde39301c37a3376572
krupeshsai <krupeshsai@gmail.com>
2021-09-13 15:58:20 +0530

    customStyles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors';
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f3e5f5"
+    }, secondary : purple
+  },
+    typography:{
+      fontFamily:"Quicksand",
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 600,
+      fontWeightBold: 700,
+    }
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -16,6 +36,7 @@ function App() {
         
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,43 +3,24 @@ import {Typography} from "@material-ui/core"
 import {Button} from '@material-ui/core';
 import { Container } from '@material-ui/core';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  btn:{
-    fontSize: 10,
-    backgroundColor:"green",
-    "&:hover" :{
-      backgroundColor:"blue"
-    }
-  },
-  title :{
-    textDecoration: "underline",
-    marginBottom: 20
-  }
-})
-
 
 export default function Create() {
-  const classes = useStyles()
-
   return (
     <Container>
       <Typography 
-      className={classes.title}
       variant ="h6"
        color = "textSecondary" 
        component = "h2"
       gutterBottom>Create a New Note</Typography>
 
-      <Button 
-      className={classes.btn}
-      onClick= {()=>console.log("You clicked me")}
-      type ="submit" 
-      color ="secondary"
-      variant="contained"
-      disableElevation
-      endIcon={<KeyboardArrowRightIcon/>}>Submit</Button>
+        <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit" 
+        color="secondary" 
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}>
+        Submit
+      </Button>
 
 
     </Container>
