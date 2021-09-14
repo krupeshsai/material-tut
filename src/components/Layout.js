35b77eeecc248c8a9299e4148c614ba338fb94d9
krupeshsai <krupeshsai@gmail.com>
2021-09-14 17:25:23 +0530

    drawer-and-makeStyle

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,20 @@
-import { makeStyles } from "@material-ui/core";
+import { Drawer, makeStyles, Typography } from "@material-ui/core";
+
+const drawerWidth = 240;
 
 const useStyle = makeStyles({
     page:{
         background : "#f9f9f9",
         width:"100%"
+    },
+    drawer:{
+        width: drawerWidth
+    },
+    drawerPaper : {
+        width: drawerWidth
+    },
+    root:{
+        display : "flex"
     }
 })
 
@@ -11,7 +22,18 @@ const Layout = ({ children }) => {
 
     const classes = useStyle()
     return (
-        <div>
+        <div className= {classes.root}>
+        <Drawer 
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{paper: classes.drawerPaper}}>
+            <div>
+                <Typography variant="h5">
+                    Super Notes
+                </Typography>
+            </div>
+        </Drawer>
 
 
         <div className={classes.page}>{children}</div>
