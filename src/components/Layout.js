2c55364d077eec5acab8e604d388da2a708f5bef
krupeshsai <krupeshsai@gmail.com>
2021-09-14 19:12:05 +0530

    AppBar-and-date-fns

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,44 @@
 import { Drawer, makeStyles, Typography } from "@material-ui/core";
 import { List , ListItem , ListItemIcon , ListItemText} from "@material-ui/core"
-
+import { AppBar, Toolbar } from "@material-ui/core"
 import { AddCircleOutlineOutlined, SubjectOutlined } from "@material-ui/icons";
 import { useHistory, useLocation } from "react-router";
-
+import { format } from "date-fns";
 const drawerWidth = 240;
 
-const useStyle = makeStyles({
-    page:{
-        background : "#f9f9f9",
-        width:"100%"
-    },
-    drawer:{
-        width: drawerWidth
-    },
-    drawerPaper : {
-        width: drawerWidth
-    },
-    root:{
-        display : "flex"
-    },
-    active:{
-        background:"#f4f4"
-    }
+const useStyle = makeStyles((theme)=> {
+    return{
+
+        page:{
+            background : "#f9f9f9",
+            width:"100%",
+            padding: theme.spacing(3)
+    
+        },
+        drawer:{
+            width: drawerWidth
+        },
+        drawerPaper : {
+            width: drawerWidth
+        },
+        root:{
+            display : "flex"
+        },
+        active:{
+            background:"#f4f4"
+        },
+        title: {
+            padding: theme.spacing(2),
+          },
+          appBar:{
+            width: `calc(100% - ${drawerWidth}px)`
+          },
+          toolBar: theme.mixins.toolbar,
+          date:{
+              flexGrow: 1
+          }
+}
+   
 })
 
 const Layout = ({ children }) => {
@@ -48,13 +64,21 @@ const Layout = ({ children }) => {
 
     return (
         <div className= {classes.root}>
+        {/* AppBar */}
+        <AppBar className={classes.appBar} elevation={0}>
+            <Toolbar>
+                <Typography className={classes.date}>{format(new Date() , "do MMMM Y")}</Typography>
+                <Typography>Sammy</Typography>
+            </Toolbar>
+        </AppBar>
+
         <Drawer 
         className={classes.drawer}
         variant="permanent"
         anchor="left"
         classes={{paper: classes.drawerPaper}}>
             <div>
-                <Typography variant="h5">
+                <Typography variant="h5" className={classes.title}>
                     Super Notes
                 </Typography>  
             </div>
@@ -76,7 +100,11 @@ const Layout = ({ children }) => {
         </Drawer>
 
 
-        <div className={classes.page}>{children}</div>
+        <div className={classes.page}>
+        <div className={classes.toolBar}></div>
+        {children}
+        </div>
+       
         </div>
     )
 }
